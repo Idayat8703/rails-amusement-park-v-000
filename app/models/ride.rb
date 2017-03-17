969cc8e0ac804c64f7a76f510b715488b52f3b36
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2017-03-17 14:15:10 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,21 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if user.tickets < attraction.tickets && user.height < attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.height < attraction.min_height
+      "Sorry. You are not tall enough to ride the Roller Coaster."
+    elsif user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the Roller Coaster."
+    else
+      user.tickets -= attraction.tickets
+      user.nausea += attraction.nausea_rating
+      user.happiness += attraction.happiness_rating
+      user.save
+      "Thanks for riding the #{attraction.name}!"
+  end
+end 
   # write associations here
 end
